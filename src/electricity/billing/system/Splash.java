5846c8377f4b0dfb140dce32d5c2e5ed330e38cb
Sonali Jain <sonalijain1604@gmail.com>
2025-07-24 23:11:05 +0530

    Add files via upload

---
+++ src/electricity/billing/system/Splash.java
@@ -0,0 +1,51 @@
+
+package electricity.billing.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame implements Runnable {
+    Thread t;
+    Splash() {
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(600, 500, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+        
+        setVisible(true);
+        
+        int x = 1;
+        for (int i = 2; i < 500; i+=4, x+=1) {
+            setSize(i + x, i);
+            setLocation(800 - ((i + x)/2), 400 - (i/2));
+            try {
+                Thread.sleep(5);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        t = new Thread(this);
+        t.start();
+        
+        setVisible(true);
+    }
+    
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            setVisible(false);
+            
+            // login frame
+            new Login();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
